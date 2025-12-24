0edf2703f46f1a4d4a556ac723d4d87c6b5bd098
Norbert Sári <sarinorbert555@gmail.com>
2025-12-24 11:16:51 +0100

    removed cache

--- obj/Debug/net10.0/AOC2025.AssemblyInfo.cs
+++ obj/Debug/net10.0/AOC2025.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AOC2025")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80e332dcccd48b9b0f3c00c7cff8c2cd19381539")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c25e149f7c9e056a5150b216e72da7595dc6511")]
 [assembly: System.Reflection.AssemblyProductAttribute("AOC2025")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AOC2025")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
